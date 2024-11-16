ea5ae9ed6a1db41b6d95adb4611f89710fdc4f1b
NRobot10Modding <nathanr11.ingham@outlook.com>
2024-11-16 20:09:27 +0000

    2ND cOMMIT

---
+++ src/main/java/net/nrobot10/moreminerals/MoreMineralsClient.java
@@ -0,0 +1,10 @@
+package net.nrobot10.moreminerals;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MoreMineralsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
