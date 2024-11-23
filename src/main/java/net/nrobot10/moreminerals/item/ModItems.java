fbd39065862e057a0ba5eec5dd226a6917c5e028
NRobot10Modding <nathanr11.ingham@outlook.com>
2024-11-23 18:03:09 +0000

    Started adding items

---
+++ src/main/java/net/nrobot10/moreminerals/item/ModItems.java
@@ -0,0 +1,12 @@
+package net.nrobot10.moreminerals.item;
+
+import net.nrobot10.moreminerals.MoreMinerals;
+
+public class ModItems {
+
+
+
+    public static void registerModItems () {
+        MoreMinerals.LOGGER.info("Registering Mod Items for " + MoreMinerals.MOD_ID);
+    }
+}
