8f0d10f4dc8dadd8fc44dd181c735808be95a1b3
NRobot10Modding <nathanr11.ingham@outlook.com>
2024-11-23 19:35:23 +0000

    Added a custom item

--- src/main/java/net/nrobot10/moreminerals/MoreMinerals.java
+++ src/main/java/net/nrobot10/moreminerals/MoreMinerals.java
@@ -2,6 +2,7 @@ package net.nrobot10.moreminerals;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.nrobot10.moreminerals.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -13,5 +14,6 @@ public class MoreMinerals implements ModInitializer {
 	public void onInitialize() {
 
 		LOGGER.info("Hello Fabric world!");
+		ModItems.registerModItems();
 	}
 }
--- src/main/java/net/nrobot10/moreminerals/item/ModItems.java
+++ src/main/java/net/nrobot10/moreminerals/item/ModItems.java
@@ -1,12 +1,26 @@
 package net.nrobot10.moreminerals.item;
 
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
 import net.nrobot10.moreminerals.MoreMinerals;
 
 public class ModItems {
 
-    
+    public static final Item GRAPHITE = registerItem("graphite", new Item(new Item.Settings()));
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, Identifier.of(MoreMinerals.MOD_ID, name), item);
+    }
 
     public static void registerModItems () {
         MoreMinerals.LOGGER.info("Registering Mod Items for " + MoreMinerals.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.NATURAL).register(entries -> {
+            entries.add(GRAPHITE);
+        });
     }
 }
