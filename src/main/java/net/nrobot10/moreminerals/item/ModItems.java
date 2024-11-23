a40c1b4d513b4216c17bd1f3f27435c55a17729a
NRobot10Modding <nathanr11.ingham@outlook.com>
2024-11-23 18:24:16 +0000

    Updated Mod_Version in gradle.properties

--- src/main/java/net/nrobot10/moreminerals/item/ModItems.java
+++ src/main/java/net/nrobot10/moreminerals/item/ModItems.java
@@ -4,7 +4,7 @@ import net.nrobot10.moreminerals.MoreMinerals;
 
 public class ModItems {
 
-
+    
 
     public static void registerModItems () {
         MoreMinerals.LOGGER.info("Registering Mod Items for " + MoreMinerals.MOD_ID);
