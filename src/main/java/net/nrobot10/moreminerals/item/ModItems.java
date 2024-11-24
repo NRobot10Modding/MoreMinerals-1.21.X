189c23c7d38b51fc925c77e7bcbd62455eeafacd
NRobot10Modding <nathanr11.ingham@outlook.com>
2024-11-24 12:32:53 +0000

    Added a custom item

--- src/main/java/net/nrobot10/moreminerals/item/ModItems.java
+++ src/main/java/net/nrobot10/moreminerals/item/ModItems.java
@@ -12,6 +12,8 @@ public class ModItems {
 
     public static final Item GRAPHITE = registerItem("graphite", new Item(new Item.Settings()));
 
+    public static final Item RAW_AMBER = registerItem("raw_amber", new Item(new Item.Settings()));
+
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registries.ITEM, Identifier.of(MoreMinerals.MOD_ID, name), item);
     }
@@ -21,6 +23,7 @@ public class ModItems {
 
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.NATURAL).register(entries -> {
             entries.add(GRAPHITE);
+            entries.add(RAW_AMBER);
         });
     }
 }
